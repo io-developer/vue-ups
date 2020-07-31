99c872e6c5674e8b057a0d150d83256f7ebbb414
io-developer <i.o.developer@gmail.com>
2020-08-01 01:31:00 +0300

    feat: add i18n core

--- public/main.js
+++ public/main.js
@@ -1,13 +1,22 @@
 import Vue from "vue";
+import VueI18n from 'vue-i18n';
 import Ups from "../src/Ups";
+import locales from "../src/locales.js";
+
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+    locale: 'en',
+    messages: locales,
+});
 
 new Vue({
+    i18n,
     el: "#ups",
     render: h => h(Ups, {
         props: {
             staticClass: 'block block--bg',
         //  mode: 'test',
-            locale: 'ru_RU',
             modelOptions: {
             //  websocketUri: 'ws://home-nas.iodev:3560/ws',
                 websocketUri: 'ws://localhost:8001/ws',
